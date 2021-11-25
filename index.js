1635c01c953a5f26fc52fe7f145832a0c648e33f
Alex Martin <alex.pmartin1@gmail.com>
2021-11-24 21:13:16 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Write your code here!
+//main.remove()
+
+document.querySelector(`main`).remove()
+
+//console.log()
+
+// var newHeader = document.getElementById(h1)
+// h1.innerHTML = `victory`
+
+var newHeader = document.createElement(`h1`)
+
+newHeader.id = `victory`
+
+// const element = document.createElement("div");
+
+
+// element.className = "pet-listing dog";
+
+newHeader.innerHTML = `Alex is the champion`
